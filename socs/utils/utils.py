95e2212a8c9182c6d218cf9b318e8def00c59a5f
jpbryan13 <jp.bryan13@gmail.com>
2026-04-02 16:59:47 -0500

    Improved documentation in utils.py

--- socs/utils/utils.py
+++ socs/utils/utils.py
@@ -2,6 +2,19 @@ import copy
 import numpy as np
 
 def row_normalize(A):
+    """
+    Computes a row-normalized copy of the input array A
+
+    Parameters
+    ----------
+    A: np.ndarray
+        Array to be row-normalized
+    
+    Returns
+    -------
+    A_n: np.ndarray
+        
+    """
     A_n = copy.deepcopy(A)
     for x in range(A.shape[0]):
         A_n[x,:] = A_n[x,:]/np.sum(A_n[x,:])
