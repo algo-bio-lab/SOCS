1d07e5e0a42a2ad09bb319940afd1551db829e78
jpbryan13 <jp.bryan13@gmail.com>
2026-04-05 20:23:59 -0500

    High-level DE analysis function

--- socs/analysis/analysis.py
+++ socs/analysis/analysis.py
@@ -455,7 +455,7 @@ def structs_to_cells(adata,struct_key='struct'):
         adata_r.obs['struct_'+y] = cell_struct_data
     return adata_r
 
-def mapping_DE(adata_src,adata_tgt,obs_key_src,obs_key_tgt,obs_val_src,obs_val_tgt_1,obs_val_tgt_2,T,expr_key_src=None,min_fc,min_q,plot_volcano=False):
+def mapping_DE(adata_src,adata_tgt,obs_key_src,obs_key_tgt,obs_val_src,obs_val_tgt_1,obs_val_tgt_2,T,expr_key_src=None,plot_volcano=False):
     """
     Identifies differentially expressed genes in a population of cells in the t0 dataset, based on the population of cells in the t1 dataset
     to which the t0 cells map.
@@ -480,15 +480,15 @@ def mapping_DE(adata_src,adata_tgt,obs_key_src,obs_key_tgt,obs_val_src,obs_val_t
         Transport map from source distribution to target distribution.
     expr_key_src: str
         Key in adata_src.obsm in which raw gene expression data is stored. If None, adata_src.X is used.
-    min_fc: float
-
-    min_q: float
-
     plot_volcano: bool
         If true, make a volcano plot depicting the differential expression between the two populations in adata_src.
     Returns
     -------
-
+    logfc: np.ndarray
+        Log-2 fold change in gene expression between cells in the source dataset that map to obs_val_tgt_1 and cells in the source
+        dataset that map to obs_val_tgt_2, in the same order of the genes in adata_src.var_names.
+    qvals: np.ndarray
+        -log10 of the corrected p-values of the differential expression analysis.
     """
     if expr_key_src is None:
         X_src = adata_src.X
@@ -516,6 +516,12 @@ def mapping_DE(adata_src,adata_tgt,obs_key_src,obs_key_tgt,obs_val_src,obs_val_t
     inds_lfc = np.where(np.abs(logfc)<10)[0] #filter out outlier DEGs
     logfc_l = logfc[inds_lfc]
     qvals_l = logfc[inds_lfc]
+    inds_degs = np.where(np.logical_and(qvals_l>min_q,np.abs(logfc_l)>min_fc))[0]
+    if(plot_volcano):
+        plt.scatter(logfc_l,qvals_l,s=5,c='#DDDDDD')
+        plt.scatter(logfc_l[inds_degs],qvals_l[inds_degs],s=5,c='red')
+    return logfc,qvals
+    
 
 
 
